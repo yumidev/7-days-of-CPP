a4010292a6aac6b4dbaa1896c2924916bb1fa33a
yumidev <yumi.ko.cheers@gmail.com>
2019-08-07 23:19:37 +0900

    sum of natural numbers

---
+++ test/naturalSum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int sum(int);
+  int n, result;
+
+  cout << "Enter a positive number bigger than 1 : ";
+  cin >> n;
+
+  if (n <= 1) {
+    cout << "Your number is not bigger than 1";
+    return 0;
+  }
+  
+  result = sum(n);
+
+  cout << "Sum of the natural numbers from "<<n<<" to 1 is "<<result<<endl;
+  return 0;
+}
+
+int sum(int n) {
+  if (n < 0)
+    return -1;
+  if (n == 0)
+    return 0;
+  else {
+    return n + sum(n-1);
+  }
+}
